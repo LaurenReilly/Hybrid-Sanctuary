ac1d54bfaf44f7f506e6bd044bcd2864ebc6799b
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-11-02 16:28:47 -0400

    abstracting the animals and food and interactions

---
+++ data.js
@@ -0,0 +1,169 @@
+var foods = [
+    {
+    name: "persimmon",
+    image: ""
+    },
+    {
+    name: "sardine",
+    image: ""
+    },
+    {
+    name: "spinach",
+    image: ""
+    },
+    {
+    name: "tofu",
+    image: ""
+    },
+    {
+    name: "grass",
+    image: ""
+    }  
+];
+
+var interactions = [
+    {
+    name: "pet",
+    image: ""
+    },
+    {
+    name: "brush",
+    image: ""
+    },
+    {
+    name: "dance with",
+    image: ""
+    },
+    {
+    name: "serenade",
+    image: ""
+    },
+    {
+    name: "play with",
+    image: ""
+    }  
+];
+
+var animals = [
+	{
+     name: "Sealurgord",
+	 image: "",
+	 sound: "",
+	 food: "sardine",
+	 foodMeter: 50,
+	 interaction: "play with",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Liorboid",
+	 image: "",
+	 sound: "",
+	 food: "tofu",
+	 foodMeter: 50,
+	 interaction: "brush",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Monkorbitor",
+	 image: "",
+	 sound: "",
+	 food: "persimmon",
+	 foodMeter: 50,
+	 interaction: "dance with",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Lemurbin",
+	 image: "",
+	 sound: "",
+	 food: "persimmon",
+	 foodMeter: 50,
+	 interaction: "pet",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Zebuuk",
+	 image: "",
+	 sound: "",
+	 food: "grass",
+	 foodMeter: 50,
+	 interaction: "brush",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Bearog",
+	 image: "",
+	 sound: "",
+	 food: "spinach",
+	 foodMeter: 50,
+	 interaction: "play with",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Alligonduro",
+	 image: "",
+	 sound: "",
+	 food: "sardine",
+	 foodMeter: 50,
+	 interaction: "serenade",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Flamzingu",
+	 image: "",
+	 sound: "",
+	 food: "sardine",
+	 foodMeter: 50,
+	 interaction: "serenade",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Wolfawab",
+	 image: "",
+	 sound: "",
+	 food: "tofu",
+	 foodMeter: 50,
+	 interaction: "serenade",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+	{
+     name: "Hipporontingus",
+	 image: "",
+	 sound: "",
+	 food: "grass",
+	 foodMeter: 50,
+	 interaction: "dance with",
+	 interactionMeter: 50, 
+	 timeBeforeLeaving: "12s",
+	 isTamed: false,
+	 escaped: false
+    },
+];
+
